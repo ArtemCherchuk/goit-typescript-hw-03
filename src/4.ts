2a79f2da49ef9c7eed875c950e5236a4bf1632c6
ArtemCherchuk <250artem250@gmail.com>
2023-12-09 13:13:32 +0100

    compeleted tasks 1 - 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
